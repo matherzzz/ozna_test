ad3670c6c3d413cada3ac95f2cb60f0e9bd03a67
matherzzz <karpoff.matvej@yandex.ru>
2023-11-07 17:44:35 +0500

    fix

--- main.py
+++ main.py
@@ -3,14 +3,60 @@ import matplotlib.pyplot as plt
 import imageio
 import os
 
-f = open('data.txt','w')
-tau = 0.01
-h = 0.01
-x = np.arange(0, 1+h, h)
-t = np.arange(0, 1+tau, tau)
 
-u = np.zeros(101 * len(x)).reshape(101, len(x))
-for i in range(101):
+tau = 600
+h = 0.1
+x = np.arange(0, 10+h, h)
+def exact_solution(x_i, t, a):
+    u = np.zeros(len(x))
+    for i in range(len(x)):
+        u[i] = x[i] * x[i] * np.exp(-(x_i - x[i]) * (x_i - x[i]) / (4 * a * a * t))
+    
+    return np.trapz(u, x) / (2 * a * np.sqrt(np.pi * t))
+    
+
+
+def diagonal(A):
+	C = make_coef(A)
+	answ = list()
+	answ.append(C[1][len(A) - 1])
+	for i in range(2, len(A) + 1):
+		answ.append(C[0][len(A) - i] * answ[i - 2] + C[1][len(A) - i])
+	return answ
+
+
+def make_coef(A):  
+    v = 0.0
+    C = list()
+    alpha = list()
+    beta = list()
+    alpha.append(-A[0][2] / A[0][1])
+    beta.append(A[0][3] / A[0][1])
+    for i in range(1, len(A)):
+        v = -A[i][1] - A[i][0] * alpha[i - 1]
+        if i == len(A) - 1:
+            alpha.append(0)
+        else:
+            alpha.append(A[i][2] / v)
+     
+        beta.append((A[i][0] * beta[i - 1] - A[i][3]) / v)
+    
+    C.append(alpha)
+    C.append(beta)
+    return C
+
+
+def norma(u):
+     u_0 = np.arange(0, 101)
+     result = u_0 - u
+     result = result ** 2
+     s = np.sum(result)
+     return np.sqrt(s)
+
+
+f = open('data.txt','w')
+u = np.zeros(2 * len(x)).reshape(2, len(x))
+for i in range(2):
     u[i][100] = 100
 
 a = 23e-6
@@ -21,22 +67,41 @@ plt.ylabel('T, °C')
 plt.savefig("./image/output0000.png")
 number = 1
 plt.close()
-for j in range(1, len(x)):
-    f.write(f"{j * h} 0 0\n")
+eps = 10
+norm = 100
+i = 0
+while norm > eps:
+    i += 1
+    if i % 6 == 0:
+         f.write(f"0 {i / 6} 0\n")
+    step = len(x)
+    coeff = tau * a / (h * h)
+    A = np.zeros(4 * (step - 2)).reshape((step - 2), 4)
+    A[0][1] = 1 + 2 * coeff
+    A[0][2] = -coeff
+    A[0][3] = u[i % 2 - 1][1] + coeff * u[i % 2][0]
+    for j in range(1, step-3):
+        A[j][0] = -coeff
+        A[j][1] = 1 + 2 * coeff
+        A[j][2] = -coeff
+        A[j][3] = u[i % 2 - 1][j + 1]
+        
+    A[step-3][0] = -coeff
+    A[step-3][1] = 1 + 2 * coeff
+    A[step-3][3] = u[i % 2 - 1][step - 2] + coeff * u[i % 2][step - 1]
+	
+    answ = diagonal(A)
+    for j in range(1, len(answ) + 1):
+        u[i % 2][j] = answ[-j]
+        if i % 6 == 0:
+             f.write(f"{j * h} {i / 6} {u[i % 2][j]}\n")
 
-for i in range(1, (len(t)- 1) * 18000 + 1):
-    if i % 1800 == 0:
-        f.write(f"0 {i / 6000} 0\n")
-    for j in range(1, len(x) - 1):
-        u[i % 101][j] = u[i % 101-1][j] + tau * a * (u[i % 101-1][j+1] - 2 * u[i % 101-1][j] + u[i % 101-1][j-1]) / (h * h)
-        if i % 1800 == 0:
-            f.write(f"{j * h} {i / 6000} {u[i % 101][j]}\n")
-    
-    if i % 1800 == 0:
-        f.write(f"1 {i / 6000} 100\n\n")
-    if i % 30000 == 0:
-        plt.plot(x, u[0])
-        plt.title(f"Время: {i / 6000} мин")
+    if i % 6 == 0:
+         f.write(f"10 {i / 6} 100\n\n")
+    norm = norma(u[i % 2])
+    if i % 30 == 0:
+        plt.plot(x, u[i % 2])
+        plt.title(f"Время: {i / 6} часов")
         plt.xlabel('x, м')
         plt.ylabel('T, °C')
         plt.savefig("./image/output" + '0' * (4-len(str(number))) + str(number)  +".png")
